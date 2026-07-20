c4c7052264c4a0495c308fcf42f5760350b06e29
Iboroaniekan <iboroaniekan40@gmail.com>
2026-07-20 09:07:57 +0100

    update made to cloudinary storage

--- project/project/settings.py
+++ project/project/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static", ]
 STATIC_ROOT = BASE_DIR / "assest"
-#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -170,7 +170,7 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 JAZZMIN_SETTINGS = {
     "top_save_buttons": False, 
